20caea49568b36c9520a0cd84445b5ac3d25476d
ccdipersio <ccdipersio@gmail.com>
2017-02-06 19:49:09 -0500

    initial commit

---
+++ conjugation_utils.c
@@ -0,0 +1,176 @@
+void setInitialConjVerb(char (*conjVerb)[30], char (*infinVerb)[30], char(*subPronoun)[5], char (*separablePrefix)[10], char (*inseparablePrefix)[10]) {
+    prefixSet(infinVerb, separablePrefix, inseparablePrefix);
+
+    int infinVerbLen = strlen(*infinVerb);
+    int verbEndingIndex;
+    char type;
+
+    if (strcmp(*subPronoun, "DU") == 0 || strcmp(*subPronoun, "ER") == 0 || strcmp(*subPronoun, "SIE") == 0 || strcmp(*subPronoun, "ES") == 0)
+        type = stemChangeCheck(infinVerb);
+
+    if (strcmp(*infinVerb, "SEIN") == 0)
+        verbEndingIndex = 0;
+    else if (strcmp(*infinVerb, "TUN") == 0)
+        verbEndingIndex = 1;
+    else if ((*infinVerb)[infinVerbLen - 2] == 'E' && (*infinVerb)[infinVerbLen - 1] == 'N')
+        verbEndingIndex = 2;
+    else
+        verbEndingIndex = 3;
+
+    for (int i = 0; i < infinVerbLen - verbEndingIndex; i++)
+        (*conjVerb)[i] = (*infinVerb)[i];
+    (*conjVerb)[infinVerbLen - verbEndingIndex] = '\0';
+
+    if (type == 'e') {
+        for (int i = 0; i < strlen(*conjVerb); i++) {
+            if ((*conjVerb)[i] == 'E') {
+                (*conjVerb)[i] = 'I';
+                break;
+            }
+        }
+    } else if (type == 'a' || type == 'i') {
+        int conjVerbLen = strlen(*conjVerb);
+        (*conjVerb)[conjVerbLen + 1] = '\0';
+        for (int i = conjVerbLen; i > 0; i--) {
+            if ((*conjVerb)[i] == 'A' || (*conjVerb)[i] == 'E') {
+                if (type == 'a')
+                    (*conjVerb)[i + 1] = 'E';
+                else if (type == 'i') {
+                    (*conjVerb)[i] = 'I';
+                    (*conjVerb)[i + 1] = 'E';
+                }
+            } else
+                (*conjVerb)[i] = (*conjVerb)[i - 1];
+        }
+    }
+}
+
+void prefixSet(char (*infinVerb)[30], char (*separablePrefix)[10], char (*inseparablePrefix)[10]) { 
+	if((*infinVerb)[0] == 'A' && (*infinVerb)[1] == 'B') {
+		(*separablePrefix)[0] = 'A'; (*separablePrefix)[1] = 'B'; (*separablePrefix)[2] = '\0';
+	} else if((*infinVerb)[0] == 'A' && (*infinVerb)[1] == 'N') { 
+		(*separablePrefix)[0] = 'A'; (*separablePrefix)[1] = 'N'; (*separablePrefix)[2] = '\0';
+	} else if((*infinVerb)[0] == 'A' && (*infinVerb)[1] == 'U' && (*infinVerb)[2] == 'F') { 
+		(*separablePrefix)[0] = 'A'; (*separablePrefix)[1] = 'U'; (*separablePrefix)[2] = 'F'; (*separablePrefix)[3] = '\0';
+	} else if((*infinVerb)[0] == 'A' && (*infinVerb)[1] == 'U' && (*infinVerb)[2] == 'S') { 
+		(*separablePrefix)[0] = 'A'; (*separablePrefix)[1] = 'U'; (*separablePrefix)[2] = 'S'; (*separablePrefix)[3] = '\0';
+	} else if((*infinVerb)[0] == 'B' && (*infinVerb)[1] == 'E' && (*infinVerb)[2] == 'I') { 
+		(*separablePrefix)[0] = 'B'; (*separablePrefix)[1] = 'E'; (*separablePrefix)[2] = 'I'; (*separablePrefix)[3] = '\0';
+	} else if((*infinVerb)[0] == 'D' && (*infinVerb)[1] == 'U' && (*infinVerb)[2] == 'R' && (*infinVerb)[3] == 'C' && (*infinVerb)[4] == 'H') { 
+		(*separablePrefix)[0] = 'D'; (*separablePrefix)[1] = 'U'; (*separablePrefix)[2] = 'R'; (*separablePrefix)[3] = 'C'; (*separablePrefix)[4] = 'H'; (*separablePrefix)[5] = '\0';
+	} else if((*infinVerb)[0] == 'E' && (*infinVerb)[1] == 'I' && (*infinVerb)[2] == 'N') { 
+		(*separablePrefix)[0] = 'E'; (*separablePrefix)[1] = 'I'; (*separablePrefix)[2] = 'N'; (*separablePrefix)[3] = '\0';
+	} else if((*infinVerb)[0] == 'F' && (*infinVerb)[1] == 'O' && (*infinVerb)[2] == 'R' && (*infinVerb)[3] == 'T') { 
+		(*separablePrefix)[0] = 'F'; (*separablePrefix)[1] = 'O'; (*separablePrefix)[2] = 'R'; (*separablePrefix)[3] = 'T'; (*separablePrefix)[4] = '\0';
+	} else if((*infinVerb)[0] == 'M' && (*infinVerb)[1] == 'I' && (*infinVerb)[2] == 'T') { 
+		(*separablePrefix)[0] = 'M'; (*separablePrefix)[1] = 'I'; (*separablePrefix)[2] = 'T'; (*separablePrefix)[3] = '\0';
+	} else if((*infinVerb)[0] == 'N' && (*infinVerb)[1] == 'A' && (*infinVerb)[2] == 'C' && (*infinVerb)[3] == 'H') {
+		(*separablePrefix)[0] = 'N'; (*separablePrefix)[1] = 'A'; (*separablePrefix)[2] = 'C'; (*separablePrefix)[3] = 'H'; (*separablePrefix)[4] = '\0';
+	} else if((*infinVerb)[0] == 'V' && (*infinVerb)[1] == 'O' && (*infinVerb)[2] == 'R') { 
+		(*separablePrefix)[0] = 'V'; (*separablePrefix)[1] = 'O'; (*separablePrefix)[2] = 'R'; (*separablePrefix)[3] = '\0';
+	} else if((*infinVerb)[0] == 'W' && (*infinVerb)[1] == 'E' && (*infinVerb)[2] == 'G') { 
+		(*separablePrefix)[0] = 'W'; (*separablePrefix)[1] = 'E'; (*separablePrefix)[2] = 'G'; (*separablePrefix)[3] = '\0';
+	} else if((*infinVerb)[0] == 'Z' && (*infinVerb)[1] == 'U') { 
+		(*separablePrefix)[0] = 'Z'; (*separablePrefix)[1] = 'U'; (*separablePrefix)[2] = '\0';
+	} else if((*infinVerb)[0] == 'Z' && (*infinVerb)[1] == 'U' && (*infinVerb)[2] == 'R' && (*infinVerb)[3] == 'U' && (*infinVerb)[4] == 'E' && (*infinVerb)[5] == 'C' && (*infinVerb)[6] == 'K') {
+		(*separablePrefix)[0] = 'Z'; (*separablePrefix)[1] = 'U'; (*separablePrefix)[2] = 'R'; (*separablePrefix)[3] = 'U'; (*separablePrefix)[4] = 'E'; (*separablePrefix)[5] = 'C'; (*separablePrefix)[6] = 'K'; (*separablePrefix)[7] = '\0';
+	} else if((*infinVerb)[0] == 'Z' && (*infinVerb)[1] == 'U' && (*infinVerb)[2] == 'S' && (*infinVerb)[3] == 'A' && (*infinVerb)[4] == 'M' && (*infinVerb)[5] == 'M' && (*infinVerb)[6] == 'E' && (*infinVerb)[7] == 'N') { 
+		(*separablePrefix)[0] = 'Z'; (*separablePrefix)[1] = 'U'; (*separablePrefix)[2] = 'S'; (*separablePrefix)[3] = 'A'; (*separablePrefix)[4] = 'M'; (*separablePrefix)[5] = 'M'; (*separablePrefix)[6] = 'E'; (*separablePrefix)[7] = 'N'; (*separablePrefix)[8] = '\0';
+	} else {
+		(*separablePrefix)[0] = '\0';
+	}
+	if((*infinVerb)[0] == 'B' && (*infinVerb)[1] == 'E') { 
+		(*inseparablePrefix)[0] = 'B'; (*inseparablePrefix)[1] = 'E'; (*inseparablePrefix)[2] = '\0';
+	} else if((*infinVerb)[0] == 'E' && (*infinVerb)[1] == 'M' && (*infinVerb)[2] == 'P') { 
+		(*inseparablePrefix)[0] = 'E'; (*inseparablePrefix)[1] = 'M'; (*inseparablePrefix)[2] = 'P'; (*inseparablePrefix)[3] = '\0';
+	} else if((*infinVerb)[0] == 'E' && (*infinVerb)[1] == 'N' && (*infinVerb)[2] == 'T') { 
+		(*inseparablePrefix)[0] = 'E'; (*inseparablePrefix)[1] = 'N'; (*inseparablePrefix)[2] = 'T'; (*inseparablePrefix)[3] = '\0';
+	} else if((*infinVerb)[0] == 'E' && (*infinVerb)[1] == 'R') { 
+		(*inseparablePrefix)[0] = 'E'; (*inseparablePrefix)[1] = 'R'; (*inseparablePrefix)[2] = '\0';
+	} else if((*infinVerb)[0] == 'G' && (*infinVerb)[1] == 'E') { 
+		(*inseparablePrefix)[0] = 'G'; (*inseparablePrefix)[1] = 'E'; (*inseparablePrefix)[2] = '\0';
+	} else if((*infinVerb)[0] == 'M' && (*infinVerb)[1] == 'I' && (*infinVerb)[2] == 'S' && (*infinVerb)[3] == 'S') { 
+		(*inseparablePrefix)[0] = 'M'; (*inseparablePrefix)[1] = 'I'; (*inseparablePrefix)[2] = 'S'; (*inseparablePrefix)[3] = 'S'; (*inseparablePrefix)[4] = '\0';
+	} else if((*infinVerb)[0] == 'V' && (*infinVerb)[1] == 'E' && (*infinVerb)[2] == 'R') { 
+		(*inseparablePrefix)[0] = 'V'; (*inseparablePrefix)[1] = 'E'; (*inseparablePrefix)[2] = 'R'; (*inseparablePrefix)[3] = '\0';
+	} else if((*infinVerb)[0] == 'V' && (*infinVerb)[1] == 'O' && (*infinVerb)[2] == 'L' && (*infinVerb)[3] == 'L') { 
+		(*inseparablePrefix)[0] = 'V'; (*inseparablePrefix)[1] = 'O'; (*inseparablePrefix)[2] = 'L'; (*inseparablePrefix)[3] = 'L'; (*inseparablePrefix)[4] = '\0';
+	} else if((*infinVerb)[0] == 'Z' && (*infinVerb)[1] == 'E' && (*infinVerb)[2] == 'R') { 
+		(*inseparablePrefix)[0] = 'Z'; (*inseparablePrefix)[1] = 'E'; (*inseparablePrefix)[2] = 'R'; (*inseparablePrefix)[3] = '\0';
+	} else { 
+		(*inseparablePrefix)[0] = '\0';
+	}
+
+	if((*separablePrefix)[0] != '\0') { 
+		char temp[30]; 
+		int prefixLen = strlen(*separablePrefix); 
+		int index = 0; 
+		for(int i = prefixLen; i < strlen(*infinVerb); i++) {
+			temp[index] = (*infinVerb)[i]; 
+			index++; 
+		}
+		temp[index] = '\0'; 
+		for(int i = 0; i < strlen(temp); i++) 
+			(*infinVerb)[i] = temp[i]; 
+		(*infinVerb)[index] = '\0'; 
+	}
+	if((*inseparablePrefix)[0] != '\0') { 
+		char temp[30];
+		int prefixLen = strlen(*inseparablePrefix); 
+		int index = 0; 
+		for(int i = prefixLen; i < strlen(*infinVerb); i++) { 
+			temp[index] = (*infinVerb)[i]; 
+			index++; 
+		}
+		temp[index] = '\0'; 
+		for(int i = 0; i < strlen(temp); i++)
+			(*infinVerb)[i] = temp[i]; 
+		(*infinVerb)[index] = '\0'; 
+	}
+
+}
+
+char stemChangeCheck(char (*infinVerb)[30]) { 
+    /* CODES
+    * E = E TO I
+    * A = A TO AE
+    * I = E TO IE
+    * Z = COULDN'T LOAD LIST
+    * N = VERB NOT IN LIST
+    */
+
+    FILE* verb_list = fopen("stem_change_verbs.txt", "r"); 
+    if(verb_list == NULL) {
+        printf("ERROR! Could not load verb list!\n"); 
+        return 'Z';
+    }
+
+    char r_verb[30]; 
+    char letter; 
+
+    while(letter != EOF) { 
+        int index = 0; 
+
+        while(letter != ',') { 
+            letter = fgetc(verb_list);
+            if(letter == EOF) 
+                break; 
+            r_verb[index] = letter; 
+            index++; 
+        }
+        r_verb[index - 1] = '\0';
+
+        if(strcmp(r_verb, *infinVerb) == 0) 
+            return fgetc(verb_list); 
+        else { 
+            while(letter != '\n') { 
+                letter = fgetc(verb_list); 
+                if(letter == EOF) 
+                    break; 
+            }
+        }
+    }
+    fclose(verb_list); 
+
+    return 'N';
+}
--- gvc.c
+++ gvc.c
@@ -10,11 +10,26 @@ void capitalizeWord(char (*word)[]);
 void getInfinVerb(char (*infinVerb)[30]);
 void getSubPronoun(char (*subPronoun)[5]);
 #include "gvc_utils.c"
+// conjugation util functions
+void setInitialConjVerb(char (*conjVerb)[30], char (*infinVerb)[30], char (*subPronoun)[5], char (*separablePrefix)[10], char (*inseparablePrefix)[10]);
+void prefixSet(char (*infinVerb)[30], char (*separablePrefix)[10], char (*inseparablePrefix)[10]);
+char stemChangeCheck(char (*infinVerb)[30]);
+#include "conjugation_utils.c"
 
 
 int main(void) {
     char infinVerb[30];
     char subPronoun[5];
     setInitialValues(&infinVerb, &subPronoun);
+
+    char conjVerb[30];
+    char separablePrefix[10];
+    char inseparablePrefix[10];
+
+    setInitialConjVerb(&conjVerb, &infinVerb, &subPronoun, &separablePrefix, &inseparablePrefix);
+
+    printf("conjverb: %s\nseppre: %s\ninsep: %s\n", conjVerb, separablePrefix, inseparablePrefix);
     
+    // up to conjugation control in present tense!!
+
 }
